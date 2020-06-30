77698cd4570b5092b0bad796d9cba0a893c19834
asabou <alexmihai906@gmail.com>
2020-06-30 16:31:56 +0300

    new commit

--- src/main/resources/sql/createAndPopulateDB-prod.sql
+++ src/main/resources/sql/createAndPopulateDB-prod.sql
@@ -50,4 +50,25 @@ use heroku_fadc8a45afc8c94;
 
 -- select * from conversation;
 
-select * from message;
+-- insert into message(conversation_title,content,username_receiver, username_sender) values
+-- 	-- M-a rmarit pe holurile scolii
+-- 	('M-a urmarit pe holurile scolii','M-a urmarit pe hol si mi-a turnat lapte in cap... am ajuns acasa murdar. Desi nu l-am bagat in seama deloc, tot se ia de mine. Ce sa fac? ','r.moise','alexandru.sabou'),
+-- 	('M-a urmarit pe holurile scolii','Incearca sa soliciti ajutorul unui profesor, cu siguranta te va ajuta','alexandru.sabou','r.moise'),
+-- 	-- M-a injurat
+--     ('M-a injurat','Un coleg m-a injurat de fata cu restul colegilor. Nu mai vreau sa merg maine la scoala','a.iurec','florina.coste'),
+--     ('M-a injurat','Ai incercat sa vorbesti cu cineva despre acest lucru? Parinitii sau profesorii stiu ce s-a intamplat in ziua respectiva?','florina.coste','a.iurec'),
+--     ('M-a injurat','nu pot sa vorbesc cu parintii, vor merge la scoala si toti vor spune ca sunt un papa-lapte','a.iurec','florina.coste'),
+--     -- Nimeni nu ma intelege
+--     ('Nimeni nu ma intelege','Vreau sa mor, nimeni nu vrea sa fie prietenul meu, toti rad de mine','florina.coste','alexandra.iurec'),
+--     ('Nimeni nu ma intelege','Vrei sa fim prieteni? Vrei sa ne jucam Asphalt 9?','alexandra.iurec','florina.coste'),
+--     -- Un coleg a fost hartuit si nu am facut nimic
+--     ('Un coleg a fost hartuit si nu am facut nimic','Azi am vazut 2 colegi care il bateau pe un prieten. Nu am facut nimic, mi-a fost frica.','f.coste','raluca.moise'),
+--     ('Un coleg a fost hartuit si nu am facut nimic','este bine sa stai in afara conflictelor, dar nu trece cu vederea, ci anunta pe cineva','raluca.moise','f.coste'),
+--     -- Copilul meu a fost batut
+--     ('Copilul meu a fost batut','Azi fiul meu a venit cu ochiul umflat, nu stiu cum sa il fac sa imi spuna ce s-a intamplat. Un sfat, va rog.','flo.coste','ale.iurec'),
+--     ('Copilul meu a fost batut','Asa am patit si eu. L-am lasat pana ce am reusit sa ii citesc jurnalul si am aflat ca la scoala s-a intamplat','ale.iurec','flo.coste'),
+--     -- Au ras de mine colegii
+--     ('Au ras de mine colegii','Au ras de mine toti colegii azi, nu mai vreau sa merg la ora de educatie fizica, eu nu stiu sa joc fotbal, vreau sa imi rup piciorul si sa termin cu fotbalul','f.coste','alexandru.sabou'),
+--     ('Au ras de mine colegii','Incearca sa faci ceva care iti place, vorbeste cu parintii si spune-le ce pasiuni ai, poate esti un excelent basketbalist','alexandru.sabou','f.coste');
+--
+-- select * from message;
--- src/main/resources/static/start.py
+++ src/main/resources/static/start.py
@@ -1,5 +1,62 @@
+#import subprocess
+#import webbrowser
+#subprocess.Popen(['python', '-m','http.server','8000'])
+#chrome = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+#webbrowser.get(chrome).open('http://localhost:8000')
+
+#while True:
+#    dr = webdriver.Chrome()
+#    dr.get("http://bacalaureat.edu.ro/")
+#    time.sleep(10)
+#    dr.close()
+
+import sys
+import time
+from selenium import webdriver
 import subprocess
 import webbrowser
-subprocess.Popen(['python', '-m','http.server','8000'])
-chrome = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-webbrowser.get(chrome).open('http://localhost:8000')
+import os
+
+
+def setProperties(environment):
+    command = "copy C:\\Users\\alexandru.sabou\\Desktop\\BetterSafeThanBully\\src\\main\\resources\\application-"+environment+".properties C:\\Users\\alexandru.sabou\\Desktop\\BetterSafeThanBully\\src\\main\\resources\\application.properties"
+    os.system(command)
+
+def startLocalServer():
+    os.chdir("C:\\Users\\alexandru.sabou\\Desktop\\BetterSafeThanBully")
+    os.system("gradlew bootRun")
+
+def startLocalClient():
+    os.chdir("C:\\Users\\alexandru.sabou\\Desktop\\BetterSafeThanBully\\src\\main\\resources\\static")
+    subprocess.Popen(["python","-m","http.server","8000"])
+    chrome = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome).open('http://localhost:8000')
+
+def runLocal():
+    setProperties("dev")
+    startLocalClient()
+    startLocalServer()
+
+def deployToGithub():
+    os.chdir("C:\\Users\\alexandru.sabou\\Desktop\\BetterSafeThanBully")
+    os.system("git add .")
+    os.system('git commit -m "new commit"')
+    os.system('git push origin master')
+
+
+def runOnCloud():
+    setProperties("prod")
+    deployToGithub()
+
+def main():
+     arguments = sys.argv
+     if (arguments[1] == "dev"):
+         print("Running on localhost .............")
+         runLocal()
+     elif (arguments[1] == "prod"):
+         print("Running on cloud ..............")
+         runOnCloud()
+     else:
+         print("Invalid argument!!!!")
+
+main()
